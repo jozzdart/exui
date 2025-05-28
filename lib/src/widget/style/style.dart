04315225acd45168f5b142aca876540d45c5f510
jozz <yosefd99@gmail.com>
2025-05-28 21:14:43 +0300

    Moved spacing extensions to a dedicated folder

--- lib/src/widget/style/style.dart
+++ lib/src/widget/style/style.dart
@@ -1,6 +1,4 @@
 export 'colored_box.dart';
 export 'container.dart';
-export 'margin.dart';
-export 'padding.dart';
 export 'sized_box.dart';
 export 'visible.dart';
--- lib/src/widget/widget.dart
+++ lib/src/widget/widget.dart
@@ -1,3 +1,4 @@
 export 'interact/interact.dart';
 export 'layout/layout.dart';
 export 'style/style.dart';
+export 'spacing/spacing.dart';
