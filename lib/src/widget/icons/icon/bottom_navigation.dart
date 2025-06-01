ca7c7ca658826d3d6fdf1ccbc7c95fdaa3c37ad8
jozz <yosefd99@gmail.com>
2025-06-01 08:28:48 +0300

    Update bottom_navigation.dart

--- lib/src/widget/icons/icon/bottom_navigation.dart
+++ lib/src/widget/icons/icon/bottom_navigation.dart
@@ -1,4 +1,3 @@
-import 'package:exui/src/material.dart';
 import 'package:flutter/widgets.dart';
 
 extension BottomNavigationItemIconExtension on Icon {
@@ -7,34 +6,4 @@ extension BottomNavigationItemIconExtension on Icon {
         icon: this,
         label: label,
       );
-}
-
-extension BottomNavigationItemIconDataExtension on IconData {
-  BottomNavigationBarItem bottomNavigationItem(
-    String label, {
-    double? size,
-    double? fill,
-    double? weight,
-    double? grade,
-    double? opticalSize,
-    Color? color,
-    List<Shadow>? shadows,
-    String? semanticLabel,
-    TextDirection? textDirection,
-    bool? applyTextScaling,
-    BlendMode? blendMode,
-  }) =>
-      icon(
-        size: size,
-        fill: fill,
-        weight: weight,
-        grade: grade,
-        opticalSize: opticalSize,
-        color: color,
-        shadows: shadows,
-        semanticLabel: semanticLabel,
-        textDirection: textDirection,
-        applyTextScaling: applyTextScaling,
-        blendMode: blendMode,
-      ).bottomNavigationItem(label);
 }
